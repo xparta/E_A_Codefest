cc4461e7de514323cc146b0c9007c737e5ceb28d
xparta <tomarobaski@gmail.com>
2014-01-20 15:28:04 +0300

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EACodefest::Application.config.secret_key_base = 'f95bf3fe0d1f7cb099785c8f2308f7d5df26ba9c785a30af0d37c19ec2b438bfd57a097b519bcd0ec25b4be1e27b035c00b46afa7f1e59759c957b2292e9b291'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EACodefest::Application.config.session_store :cookie_store, key: '_E_A_Codefest_session'
